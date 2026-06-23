4aec15fe1f7f586b74e29394d404d2707c14f276
Rishi-Manish-Jotangia <jotangia.rishi317@gmail.com>
2026-06-23 07:35:32 +0530

    Enter you grades
    A python program made for entering grades and displaying them.

---
+++ enter you grades.py
@@ -0,0 +1,14 @@
+marks = int(input("What is your grade? Enter your marks: "))
+
+if marks >= 90:
+    print("Grade is A")
+elif marks >= 80:
+    print("Grade is B")
+elif marks >= 70:
+    print("Grade is C")
+elif marks >= 60:
+    print("Grade is D")
+elif marks >= 50:
+    print("Grade is E")
+elif marks >= 40:
+    print("Failed")
